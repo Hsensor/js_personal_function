51b52e8ff37892dedcdb7fe68ccb9ef9095174c1
Hsensor <1203460788@qq.com>
2016-11-01 17:57:08 +0800

    es6 pipeline redux_compose

---
+++ es6_pipeline.js
@@ -0,0 +1,19 @@
+const pipeline = (...funcs) =>
+  val => funcs.reduce((a, b) => b(a), val);
+
+const plus1 = a => a + 1;
+const mult2 = a => a * 2;
+const addThenMult = pipeline(plus1, mult2);
+
+addThenMult(5)
+
+
+/*function pipeLine(){
+	var funs = [].slice.apply(arguments);
+
+	return function(val){
+		return funs.reduce(function(a,b){
+			return b(a);
+		},val)
+	}
+}*/
